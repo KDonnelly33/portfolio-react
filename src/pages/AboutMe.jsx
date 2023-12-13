7bb4dbb9dc6586a76946cc4c2d8e1b3b32c0502a
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-12-13 14:13:25 -0600

    fixed typos in about me

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -8,11 +8,11 @@ export default function AboutMe() {
           exciting world of web development. My culinary career instilled in me
           a strong work ethic, attention to detail, and the ability to thrive in
           high-pressure environments – qualities that I now apply to my work as
-          a web developer.I attended a coding bootcamp to acquire the necessary
+          a web developer. I attended a coding bootcamp to acquire the necessary
           skills, becoming proficient in all the core languages of web
           development. Today, I'm eager to create user-friendly, visually
           appealing websites and web applications, combining my culinary
-          principles of artistry and precision with my coding expertis I'm
+          principles of artistry and precision with my coding expertise. I'm
           excited to collaborate with forward-thinking professionals and
           organizations that value creativity and continuous learning. If you're
           looking for a passionate and adaptable web developer, let's connect
